de7049e2cc3c2c036d944e988a900e3c2ca443ec
xmy <770212572@qq.com>
2018-04-03 15:49:10 +0800

    xmy

--- src/main/java/com/xmy/portal/utils/FtpClientUtils.java
+++ src/main/java/com/xmy/portal/utils/FtpClientUtils.java
@@ -2,10 +2,17 @@ package com.xmy.portal.utils;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.net.ftp.FTPClient;  
-import org.apache.commons.net.ftp.FTPReply;  
-  
-import java.io.*;  
-  
+import org.apache.commons.net.ftp.FTPReply;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
+import java.net.Socket;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.UUID;
+import java.util.logging.Logger;
+
 /** 
  * JAVA FTPClient 工具类 
  * 
@@ -116,14 +123,113 @@ public class FtpClientUtils{
         return ftpClient.retrieveFileStream(ftpFile);  
     }  
   
-    public static void main(String[] args) {  
-        try {  
+    public static void main (String[] args){
+        /*try {
             FileInputStream in=new FileInputStream(new File("G:\\img/2.jpg"));
-            //boolean flag = uploadFile("118.25.102.29", 21, "root", "celfc020639.", "/usr/local/tomcat8-upload/webapps/ROOT/img/", "333.jpg", in);
-            boolean flag = uploadFile("118.25.102.29", 21, "root", "celfc020639.", "/home/", "333.jpg", in);
+            boolean flag = uploadFile("118.25.102.29", 21, "root", "celfc020639.", "/usr/local/tomcat8-upload/webapps/ROOT/img/", "333.jpg", in);
+            //boolean flag = uploadFile("118.25.102.29", 21, "ubuntu", "celfc020639.", "/home/ubuntu/img/", "333.jpg", in);
             System.out.println(flag);
         } catch (FileNotFoundException e) {  
             e.printStackTrace();  
-        }  
-    }  
+        }  */
+
+        try {
+            URL url = new URL("http://localhost:8080/img/3.jpg");
+            URLConnection conn = url.openConnection();
+            conn.setDoOutput(true);
+            OutputStream os = conn.getOutputStream();
+            InputStream is = new FileInputStream(new File("G:\\img/5.jpg"));
+            byte[] bytes = new byte[10];
+            int length = 0;
+            while (-1 != (length = is.read(bytes))) {
+                os.write(bytes, 0, length);
+                os.flush();
+            }
+            is.close();
+            os.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        } finally {
+
+        }
+
+       /* URL url= new URL("http://localhost:8080/img/5.jpg");
+        URLConnection conn = url.openConnection();
+        InputStream is = conn.getInputStream();
+        OutputStream os = new FileOutputStream(new File("G:\\img/5.jpg"));
+        byte[] bytes = new byte[10];
+        int length = 0;
+        while(-1!=(length = is.read(bytes))){
+            os.write(bytes,0,length);
+            os.flush();
+        }
+        is.close();
+        os.close();
+*/
+
+        /*try {
+            Socket s = new Socket("localhost", 8080);//连接到服务端的端口
+            OutputStream out = s.getOutputStream();
+            upload("G:\\img/5.jpg","localhost:8080",out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+
+    }
+
+    private static void upload(String filePath, String host, OutputStream out)
+            throws IOException{
+        FileInputStream fis = new FileInputStream(filePath);
+        //模拟http put请求上传文件
+        String head = "PUT " + filePath + " HTTP/1.1\r\n" +
+                "Host: " + host + "\r\n" +
+                "Connection: keep-alive\r\n" +
+                "Accept: text/html,application/xhtml+xml,application/xml,*/*\r\n" +
+                "Accept-Encoding: gzip, deflate\r\n" +
+                "Accept-Language: zh-CN\r\n" +
+                "Content-Length: " + fis.available() + "\r\n" +
+                "\r\n";
+        out.write(head.getBytes());
+        byte[] buf = new byte[1024];
+        int len = -1;
+        while((len = fis.read(buf)) != -1){
+            out.write(buf, 0, len);
+        }
+        fis.close();
+    }
+
+    public static String uploadFile(MultipartFile file, HttpServletRequest request) throws IllegalStateException, IOException{
+        if(file!=null){
+            //获取上传文件的原始名称
+            String originalFilename = file.getOriginalFilename();
+            String newFileName ="";
+            String pic_path;
+            // 上传图片
+            if ( originalFilename != null && originalFilename.length() > 0) {
+                //获取Tomcat服务器所在的路径
+                String tomcat_path = System.getProperty( "user.dir" );
+                System.out.println(tomcat_path);
+                //获取Tomcat服务器所在路径的最后一个文件目录
+                String bin_path = tomcat_path.substring(tomcat_path.lastIndexOf("\\")+1,tomcat_path.length());
+                System.out.println(bin_path);
+                //若最后一个文件目录为bin目录，则服务器为手动启动
+                if(("bin").equals(bin_path)){//手动启动Tomcat时获取路径为：D:\Software\Tomcat-8.5\bin
+                    //获取保存上传图片的文件路径
+                    pic_path = tomcat_path.substring(0,System.getProperty( "user.dir" ).lastIndexOf("\\")) +"\\webapps"+"\\pic_file\\";
+                }else{//服务中自启动Tomcat时获取路径为：D:\Software\Tomcat-8.5
+                    pic_path = tomcat_path+"\\webapps"+"\\pic_file\\";
+                }
+                // 新的图片名称
+                newFileName = UUID.randomUUID() + originalFilename.substring(originalFilename.lastIndexOf("."));
+                // 新图片
+                File newFile = new File(pic_path + newFileName);
+                // 将内存中的数据写入磁盘
+                file.transferTo(newFile);
+            }
+            return newFileName;
+        }else{
+            return null;
+        }
+    }
 }  
